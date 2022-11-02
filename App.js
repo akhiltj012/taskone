f7162166952a1ba3e2da4ff7ac277ab27c9666c5
Akhil TJ <100039517+akhiltj012@users.noreply.github.com>
2022-11-02 16:28:55 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import { Sidebar } from './components/sidebar/Sidebar';
+import { Allroutes } from './Routes/Allroutes';
+
+
+function App() {
+  return (
+    <div className="App">
+    <Sidebar/>
+    <Allroutes/>
+
+    </div>
+  );
+}
+
+export default App;
